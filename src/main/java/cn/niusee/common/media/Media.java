a4ae028966bb288932ef8ec233a817e3e67f1d0a
Qianliang Zhang <zhangqianliang@gmail.com>
2021-09-09 15:46:23 +0800

    添加媒体信息profile、pix_fmt的获取

--- src/main/java/cn/niusee/common/media/Media.java
+++ src/main/java/cn/niusee/common/media/Media.java
@@ -35,6 +35,16 @@ public class Media {
         private String codec_type;
 
         /**
+         * 编码文件，如：Baseline、Main、High
+         */
+        private String profile;
+
+        /**
+         * 图像格式，如：yuv420p、yuvj420p
+         */
+        private String pix_fmt;
+
+        /**
          * 视频流的宽(只包含在流类型在video中)
          */
         private int width;
@@ -86,137 +96,70 @@ public class Media {
          */
         private String field_order;
 
-        /**
-         * 获取流索引
-         *
-         * @return 流索引
-         */
         public int getIndex() {
             return index;
         }
 
-        /**
-         * 获取编码格式名称
-         *
-         * @return 编码格式名称
-         */
         public String getCodecName() {
             return codec_name;
         }
 
-        /**
-         * 获取流类型
-         *
-         * @return 流类型
-         */
         public String getCodecType() {
             return codec_type;
         }
 
-        /**
-         * 获取视频流的宽
-         *
-         * @return 视频流的宽
-         */
+        public String getProfile() {
+            return profile;
+        }
+
+        public String getPixFmt() {
+            return pix_fmt;
+        }
+
         public int getWidth() {
             return width;
         }
 
-        /**
-         * 获取视频流的高
-         *
-         * @return 视频流的高
-         */
         public int getHeight() {
             return height;
         }
 
-        /**
-         * 获取视频流帧的格式
-         *
-         * @return 视频流帧的格式
-         */
         public String getFixFormat() {
             return fix_format;
         }
 
-        /**
-         * 获取视频流帧率
-         *
-         * @return 视频流帧率
-         */
         public String getFrameRate() {
             return r_frame_rate;
         }
 
-        /**
-         * 获取视频流平均帧率
-         *
-         * @return 视频流平均帧率
-         */
         public String getAvgFrameRate() {
             return avg_frame_rate;
         }
 
-        /**
-         * 获取视频流码率
-         *
-         * @return 视频流码率
-         */
         public int getBitRate() {
             return bit_rate;
         }
 
-        /**
-         * 获取音频流采样率
-         *
-         * @return 音频流采样率
-         */
         public int getSampleRate() {
             return sample_rate;
         }
 
-        /**
-         * 获取音频流通道数
-         *
-         * @return 音频流通道数
-         */
         public int getChannels() {
             return channels;
         }
 
-        /**
-         * 获取流时长(单位为秒)
-         *
-         * @return 流时长（秒）
-         */
         public double getDurationInSeconds() {
             return duration;
         }
 
-        /**
-         * 获取流时长(单位为毫秒)
-         *
-         * @return 流时长（毫秒）
-         */
         public long getDurationInMilliseconds() {
             return (long) (duration * 1000);
         }
 
-        /**
-         * 获取视频的扫描方式
-         *
-         * @return 视频的扫描方式
-         */
         public String getFieldOrder() {
             return field_order;
         }
 
-        /**
-         * 是否是逐行扫描的视频
-         *
-         * @return 是否是逐行扫描的视频
-         */
         public boolean isProgressiveVideo() {
             return "progressive".equals(field_order);
         }
@@ -247,38 +190,18 @@ public class Media {
          */
         private int bit_rate;
 
-        /**
-         * 获取媒体格式名称
-         *
-         * @return 媒体格式名称
-         */
         public String getFormatName() {
             return format_name;
         }
 
-        /**
-         * 获取媒体时长(单位为秒)
-         *
-         * @return 媒体时长（秒）
-         */
         public double getDurationInSeconds() {
             return duration;
         }
 
-        /**
-         * 获取媒体时长(单位为毫秒)
-         *
-         * @return 媒体时长（毫秒）
-         */
         public long getDurationInMilliseconds() {
             return (long) (duration * 1000);
         }
 
-        /**
-         * 获取媒体码率
-         *
-         * @return 媒体码率
-         */
         public int getBitRate() {
             return bit_rate;
         }
@@ -300,29 +223,14 @@ public class Media {
      */
     private List<Stream> streams;
 
-    /**
-     * 获取媒体格式信息
-     *
-     * @return 媒体格式信息
-     */
     public Format getFormat() {
         return format;
     }
 
-    /**
-     * 获取媒体流信息
-     *
-     * @return 媒体流信息
-     */
     public List<Stream> getStreams() {
         return streams;
     }
 
-    /**
-     * 获取媒体中的第一个视频流信息
-     *
-     * @return 视频流信息
-     */
     public Stream getFirstVideoStream() {
         if (streams != null) {
             for (Stream stream : streams) {
@@ -334,11 +242,6 @@ public class Media {
         return null;
     }
 
-    /**
-     * 获取媒体中第一个音频流信息
-     *
-     * @return 音频流信息
-     */
     public Stream getFirstAudioStream() {
         if (streams != null) {
             for (Stream stream : streams) {
